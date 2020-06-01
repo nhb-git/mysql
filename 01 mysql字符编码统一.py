1267b1f14af3be88079cf8b7875d88cabd9652cc
niuhaibao <niuhaibao@gmail.com>
2020-06-01 08:32:53 +0800

    mysql字符编码统一配置

---
+++ 01 mysql字符编码统一.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 开发人员   ：Davis Niu
+# 开发时间   ：6/1/2020  8:03 AM 
+# 文件名称   ：01 mysql字符编码统一.py
+
+
+"""
+统一mysql字符编码:
+
+[mysqld]
+character-set-server=utf-8
+collation-server=utf-8_general_ci
+
+[client]
+default-character-set=utf-8
+
+[mysql]
+default-character-set=utf-8
+"""
